7f79697cbcd2eaf0c68c236e56ae48f9c523c26b
SherlockAlex <101167329+SherlockAlex@users.noreply.github.com>
2023-01-23 19:14:57 +0800

    Fix windows

--- windows/Application.h
+++ windows/Application.h
@@ -2,16 +2,16 @@
 #ifndef NOAGAMEENGINE_APPLICATION_H
 #define NOAGAMEENGIEN_APPLICATION_H
 
-//ÏµÍ³¿â
-//#include <windows.h>
+//系统库
+#include <windows.h>
 #include <cstdio>
 
-void FreshScreen();						//Ë¢ÐÂÆÁÄ»
-void HideCursor();						//Òþ²ØÊó±ê
-void SetCursorPos(float x, float y);	//ÉèÖÃ¹â±êÎ»ÖÃ
-void SetColor(int color);				//ÉèÖÃÎÄ×ÖÑÕÉ«
-void ResetConsole();					//¿ØÖÆÌš³õÊŒ»¯
-void Quit();							//¹Ø±ÕÓŠÓÃ
-void DelaySecond();						//ÑÓÊ±ŒžÃë
+void FreshScreen();						//刷屏
+void HideCursor();						//隐藏鼠标
+void SetCursorPos(float x, float y);	//设置鼠标坐标
+void SetColor(int color);				//设置颜色
+void ResetConsole();					//重置
+void Quit();							//退出程序
+void DelaySecond();						//延时
 
 #endif
